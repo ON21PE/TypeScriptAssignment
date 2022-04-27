94ffcfc4f2cf971c4967275699bd15d1f7a5655a
Paul Eitenbichler <paul.eitenbichler@ueberbit.de>
2022-04-27 16:35:15 +0200

    added countdown display

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,37 @@
-
-const rounds = <HTMLInputElement>document.getElementById("rounds");
+let zusammengerechnet: number;
+const round = <HTMLInputElement>document.getElementById("rounds");
 const time = <HTMLInputElement>document.getElementById("time");
 const break_time = <HTMLInputElement>document.getElementById("break_time");
 const div = <HTMLDivElement>document.getElementById("test");
 
-rounds.addEventListener("change", test);
+const body = document.body;
+const counter = document.createElement("div");
+counter.innerText = "Bitte Zeit ausfüllen";
+body.append(counter);
+
+round.addEventListener("change", test);
 time.addEventListener("change", test);
 break_time.addEventListener("change", test);
 
 function test() {
-  console.log(rounds.value);
+  console.log(round.value);
   console.log(break_time.value);
   console.log(time.value);
-};
+  let alles: any =
+    parseInt(round.value) * (parseInt(time.value) + parseInt(break_time.value));
+  zusammengerechnet = alles;
+  console.log(alles);
+}
 
 let button = document.getElementById("startButton");
 console.log(button);
 button?.addEventListener("click", function () {
-  startTimer(6);
+  startTimer(zusammengerechnet);
 });
 
 function startTimer(zeit: any) {
   console.log(zeit);
+  counter.innerText = zeit;
 
   window.setTimeout(function () {
     countdown(zeit);
@@ -29,14 +39,10 @@ function startTimer(zeit: any) {
 }
 function countdown(zeit: any) {
   zeit--;
-  console.log(zeit);
+  counter.innerText = zeit;
   if (zeit != 0) {
     window.setTimeout(function () {
       countdown(zeit);
     }, 1000);
   }
 }
-
-// function zusammenrechnen(){
-//  let alles:number = rounds * (time + break_time)
-// }
