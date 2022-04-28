8e00fd627693414b3a5b55ebdcc6fa62021bb84c
Paul Eitenbichler <paul.eitenbichler@ueberbit.de>
2022-04-28 08:48:56 +0200

    addad timeconverter

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ button?.addEventListener("click", function () {
 
 function startTimer(zeit: any) {
   console.log(zeit);
-  counter.innerText = zeit;
+  counter.innerText = secondsToHms(zeit);
 
   window.setTimeout(function () {
     countdown(zeit);
@@ -39,10 +39,20 @@ function startTimer(zeit: any) {
 }
 function countdown(zeit: any) {
   zeit--;
-  counter.innerText = zeit;
+  counter.innerText = secondsToHms(zeit);
   if (zeit != 0) {
     window.setTimeout(function () {
       countdown(zeit);
     }, 1000);
   }
+}
+
+function secondsToHms(d:any) {
+  d = Number(d);
+
+  var h = Math.floor(d / 3600);
+  var m = Math.floor(d % 3600 / 60);
+  var s = Math.floor(d % 3600 % 60);
+
+  return ('0' + h).slice(-2) + ":" + ('0' + m).slice(-2) + ":" + ('0' + s).slice(-2);
 }
