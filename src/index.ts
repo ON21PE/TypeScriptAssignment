8191bd7710c9b08a7a8e773bcda5d262f08b00c2
Paul Eitenbichler <paul.eitenbichler@ueberbit.de>
2022-05-05 08:49:46 +0200

    added rountercounter

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import { skipPartiallyEmittedExpressions } from "../node_modules/typescript/lib/typescript";
+
 let zusammengerechnet: number;
+let timeUp: boolean = false;
 const round = <HTMLInputElement>document.getElementById("rounds");
 const time = <HTMLInputElement>document.getElementById("time");
 const break_time = <HTMLInputElement>document.getElementById("break_time");
@@ -6,35 +9,66 @@ const div = <HTMLDivElement>document.getElementById("test");
 
 const body = document.body;
 const counter = document.createElement("div");
+const roundDisplay = document.createElement("div");
+counter.classList.add("countdown");
+roundDisplay.classList.add("rounddisplay");
 counter.innerText = "Bitte Zeit ausfüllen";
 body.append(counter);
+body.append(roundDisplay);
 
 round.addEventListener("change", test);
 time.addEventListener("change", test);
 break_time.addEventListener("change", test);
-
 function test() {
-  console.log(round.value);
-  console.log(break_time.value);
-  console.log(time.value);
-  let alles: any =
-    parseInt(round.value) * (parseInt(time.value) + parseInt(break_time.value));
-  zusammengerechnet = alles;
-  console.log(alles);
+  console.log(round, time, break_time);
+}
+async function start() {
+  let pauseActive = false;
+  let timerStarted = false;
+  let pausen = parseInt(break_time.value);
+  let zeit = parseInt(time.value);
+  let runden = parseInt(round.value);
+
+  while (runden > 0) {
+    if (!timerStarted) {
+      startTimer(zeit);
+      timerStarted = true;
+      roundDisplay.innerText = "Runde" + " " + runden;
+    }
+    if (!timeUp) {
+      await Sleep(200);
+    }
+    if (timeUp) {
+      if (!pauseActive) {
+        startTimer(pausen);
+        pauseActive = true;
+        timerStarted = true;
+        roundDisplay.innerText = "Pause" + " " + runden;
+      } else {
+        pauseActive = false;
+        timerStarted = false;
+        runden--;
+      }
+    }
+  }
 }
 
+// Startet Counter
 let button = document.getElementById("startButton");
 console.log(button);
 button?.addEventListener("click", function () {
-  startTimer(zusammengerechnet);
+  start();
 });
 
+// Countdown
 function startTimer(zeit: any) {
+  timeUp = false;
   console.log(zeit);
   counter.innerText = secondsToHms(zeit);
 
   window.setTimeout(function () {
     countdown(zeit);
+    console.log(zeit);
   }, 1000);
 }
 function countdown(zeit: any) {
@@ -44,15 +78,22 @@ function countdown(zeit: any) {
     window.setTimeout(function () {
       countdown(zeit);
     }, 1000);
+  } else {
+    timeUp = true;
+    console.log("timeUp= true");
   }
 }
-
-function secondsToHms(d:any) {
+function Sleep(milliseconds: number) {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
+function secondsToHms(d: any) {
   d = Number(d);
 
   var h = Math.floor(d / 3600);
-  var m = Math.floor(d % 3600 / 60);
-  var s = Math.floor(d % 3600 % 60);
+  var m = Math.floor((d % 3600) / 60);
+  var s = Math.floor((d % 3600) % 60);
 
-  return ('0' + h).slice(-2) + ":" + ('0' + m).slice(-2) + ":" + ('0' + s).slice(-2);
+  return (
+    ("0" + h).slice(-2) + ":" + ("0" + m).slice(-2) + ":" + ("0" + s).slice(-2)
+  );
 }
